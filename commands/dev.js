e30ec9ca8e9da61c4c57ef4e9597b141c2ae208c
Donutgamer0684 <79782483+Donutgamer0684@users.noreply.github.com>
2021-07-06 00:08:06 -0500

    wfawfaw

---
+++ commands/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'dev',
+    description: "dev",
+    execute(message, args, Discord) {
+        message.channel.send('> ⚠ This is being worked on please wait ⚠')
+    }
+  }
