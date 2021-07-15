6df66fe087be76f909db7d0b5dc4d8b951d33912
Donutgamer0684 <79782483+Donutgamer0684@users.noreply.github.com>
2021-07-15 01:16:57 -0500

    fix

--- commands/help.js
+++ commands/help.js
@@ -2,22 +2,20 @@ module.exports = {
     name: 'help',
     description: "this is a help command",
     execute(message, args, Discord) {
-        {
-            const newEmbed = new Discord.MessageEmbed()
-            .setColor('')
-            .setTitle('')
-            .setURL('')
-            .setDescription('')
-            .addFields(
-                { name: 'EndGame Bot Commands', value: 'We are adding commands every day! If you need help you can dm ! Donut#6637' },
-                { name: '\u200B', value: '\u200B' },
-                { name: '🤣 **Fun**', value: '`.help-fun`', inline: true },
-                { name: '🕹 **Config**', value: '`.help-config`', inline: true },
-                { name: '🔨 **Utility**', value: '`.help-utility`', inline: true },
-            )
-            .setTimestamp()
-            .setFooter('EndGame | The Most Sexy BOT You Will Ever Find');
-        }
+        const newEmbed = new Discord.MessageEmbed()
+        .setColor('')
+        .setTitle('')
+        .setURL('')
+        .setDescription('')
+        .addFields(
+            { name: 'EndGame Bot Commands', value: 'We are adding commands every day! If you need help you can dm ! Donut#6637' },
+            { name: '\u200B', value: '\u200B' },
+            { name: '🤣 **Fun**', value: '`.help-fun`', inline: true },
+            { name: '🕹 **Config**', value: '`.help-config`', inline: true },
+            { name: '🔨 **Utility**', value: '`.help-utility`', inline: true },
+        )
+        .setTimestamp()
+        .setFooter('EndGame | The Most Sexy BOT You Will Ever Find');
 
         message.channel.send('Here are the EndGame BOT commands!', newEmbed);
     }
