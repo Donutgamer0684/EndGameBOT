0a9f334c0d378e5b9aef3ee89426152f4bfcbd76
Donutgamer0684 <79782483+Donutgamer0684@users.noreply.github.com>
2021-07-15 00:32:21 -0500

    Update hack.js

--- commands/hack.js
+++ commands/hack.js
@@ -9,7 +9,7 @@ module.exports = {
   
       else {
         const hack = args.slice(0).join(" ") && args.shift().toLowerCase()
-        let responses = ["IloveCookies", "Password101", "8======D", "D7gyg2ebadawh", "ILoveHotMoms", "InsertPasswordHere", "CharlieFanPagesDeserveToBurn", "MommysBoy", "SugarDaddy", "IputMyDongInAoven", "Blacklisted"]
+        let responses = ["IloveCookies", "Password101", "IforgotMyPassword", "D7gyg2ebadawh", "Milk", "InsertPasswordHere", "CharlieFanPagesDeserveToBurn", "MommysBoy", "🍦 CREAMY 🍦", "youtube.com/watch?v=dQw4w9WgXcQ", "Blacklisted"]
   
         let response = Math.floor(Math.random() * responses.length)
         message.channel.send(`Hacking... ${hack}`);
